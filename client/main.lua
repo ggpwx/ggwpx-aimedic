d987516bd5bb65fa62dfdc28981496decd9cf25b
ggpwx <shandy.sandoro.55.ss@gmail.com>
2024-09-27 15:36:49 +0700

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -9,7 +9,9 @@ RegisterCommand(Config.MedicCommand, function(source, args, raw)
         QBCore.Functions.TriggerCallback('ggwpx-aimedic:checkDoctorAvailability', function(medicsAvailable, canPay)
             if medicsAvailable >= Config.MinEMSOnline and canPay then
                 SpawnAmbulance(GetEntityCoords(currentPlayerPed))
-                Notify(Config.Notifications["onTheWay"])
+                if Config.UseVehicle then
+                    Notify(Config.Notifications["onTheWay"])
+                end
             elseif medicsAvailable < Config.MinEMSOnline then
                 Notify(Config.Notifications["tooManyEMTs"], "error")
             elseif not canPay then
@@ -25,28 +27,80 @@ end)
 
 function SpawnAmbulance(position)
     isActive = true
-    local vehicleModel = GetHashKey("ambulance")
-    RequestModel(vehicleModel)
-    while not HasModelLoaded(vehicleModel) do
-        Wait(500)
+    if Config.UseVehicle then
+        local vehicleModel = GetHashKey("ambulance")
+        RequestModel(vehicleModel)
+        local timeout = 0 
+
+        while not HasModelLoaded(vehicleModel) and timeout < 20 do
+            Citizen.Wait(500)
+            timeout = timeout + 1
+        end
+
+        if not HasModelLoaded(vehicleModel) then
+            print("Model kendaraan ambulance gagal dimuat.")
+            return
+        end
+
+        local pedModel = GetHashKey('s_m_m_doctor_01')
+        RequestModel(pedModel)
+        timeout = 0 
+        while not HasModelLoaded(pedModel) and timeout < 20 do
+            Citizen.Wait(500)
+            timeout = timeout + 1
+        end
+
+        if not HasModelLoaded(pedModel) then
+            print("Model ped medic gagal dimuat.")
+            return
+        end
+
+        local heading = GetEntityHeading(currentPlayerPed)
+        local spawnPos = GetRandomVehicleNode(position)
+        vehicle = CreateVehicle(vehicleModel, spawnPos, heading, true, false)
+        
+        if DoesEntityExist(vehicle) then
+            SetVehicleOnGroundProperly(vehicle)
+            SetVehicleNumberPlateText(vehicle, "AIMEDIC")
+            SetVehicleEngineOn(vehicle, true, true, false)
+
+            medicPed = CreatePedInsideVehicle(vehicle, 26, pedModel, -1, true, false)
+            print("Medic ped berhasil dibuat di dalam kendaraan.")
+
+            AddBlipForEntity(vehicle)
+
+            local speed = 80.0 
+            local drivingStyle = 1074528293 
+            
+            TaskVehicleDriveToCoord(medicPed, vehicle, position.x, position.y, position.z, speed, 0, vehicleModel, drivingStyle, 2.0)
+
+            if not DoesEntityExist(medicPed) then
+                print("Gagal membuat medic ped.")
+            end
+        else
+            print("Kendaraan tidak berhasil dibuat.")
+        end
+    else
+        local pedModel = GetHashKey('s_m_m_doctor_01')
+        RequestModel(pedModel)
+        local timeout = 0 
+        while not HasModelLoaded(pedModel) and timeout < 20 do
+            Citizen.Wait(500)
+            timeout = timeout + 1
+        end
+
+        if not HasModelLoaded(pedModel) then
+            print("Model ped medic gagal dimuat.")
+            return
+        end
+
+        medicPed = CreatePed(4, pedModel, position.x, position.y, position.z, 0.0, true, false)
+        TaskGoToCoordAnyMeans(medicPed, position.x, position.y, position.z, 1.0, 0, 0, 786603, 0xbf800000)
+
+        if not DoesEntityExist(medicPed) then
+            print("Gagal membuat medic ped.")
+        end
     end
-
-    local pedModel = GetHashKey('s_m_m_doctor_01')
-    RequestModel(pedModel)
-    while not HasModelLoaded(pedModel) do
-        Wait(500)
-    end
-
-    local heading = GetEntityHeading(currentPlayerPed)
-    vehicle = CreateVehicle(vehicleModel, spawnPos, heading, true, false)
-    SetVehicleOnGroundProperly(vehicle)
-    SetVehicleNumberPlateText(vehicle, "AIMEDIC")
-    SetVehicleEngineOn(vehicle, true, true, false)
-
-    medicPed = CreatePedInsideVehicle(vehicle, 26, pedModel, -1, true, false)
-    AddBlipForEntity(vehicle)
-
-    TaskVehicleDriveToCoord(medicPed, vehicle, position.x, position.y, position.z, 40.0, 0, vehicleModel, 524863, 2.0)
 end
 
 function GetRandomVehicleNode(position)
@@ -67,12 +121,20 @@ Citizen.CreateThread(function()
             local distToVehicle = Vdist(playerCoords, vehicleCoords)
             local distToPed = Vdist(playerCoords, pedCoords)
 
-            if distToVehicle > Config.SpawnDistance then
-                Notify(Config.Notifications["onTheWay"])
-            elseif distToPed > Config.SpawnDistance then
-                Notify(Config.Notifications["waitingForMedics"])
-            elseif distToPed <= Config.SpawnDistance then
-                TaskGoToCoordAnyMeans(medicPed, playerCoords.x, playerCoords.y, playerCoords.z, 1.0, 0, 0, 786603, 0xbf800000)
+            if Config.UseVehicle then
+                if distToVehicle > Config.SpawnDistance then
+                    Notify(Config.Notifications["onTheWay"])
+                elseif distToPed > Config.SpawnDistance then
+                    Notify(Config.Notifications["waitingForMedics"])
+                elseif distToPed <= Config.SpawnDistance then
+                    TaskGoToCoordAnyMeans(medicPed, playerCoords.x, playerCoords.y, playerCoords.z, 1.0, 0, 0, 786603, 0xbf800000)
+                    if distToPed <= 1 then
+                        isActive = false
+                        ClearPedTasksImmediately(medicPed)
+                        PerformMedicalTreatment()
+                    end
+                end
+            else
                 if distToPed <= 1 then
                     isActive = false
                     ClearPedTasksImmediately(medicPed)
@@ -102,7 +164,9 @@ function PerformMedicalTreatment()
         Notify(Config.Notifications["reviveComplete"] .. Config.ServiceFee, "success")
         TriggerServerEvent('ggwpx-aimedic:chargeFee')  
         DeleteEntity(medicPed)
-        DeleteEntity(vehicle)
+        if Config.UseVehicle then
+            DeleteEntity(vehicle)
+        end
     end)
 end
 
